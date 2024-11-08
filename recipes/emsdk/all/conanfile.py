13d04007827b0729727eecdec8a1df71f9015ced
Matt Angus <mattangus02@gmail.com>
2024-11-08 12:18:55 +0000

    [emsdk] bump to 3.1.71

--- recipes/emsdk/all/conanfile.py
+++ recipes/emsdk/all/conanfile.py
@@ -12,7 +12,9 @@ required_conan_version = ">=1.52.0"
 
 class EmSDKConan(ConanFile):
     name = "emsdk"
-    description = "Emscripten SDK. Emscripten is an Open Source LLVM to JavaScript compiler"
+    description = (
+        "Emscripten SDK. Emscripten is an Open Source LLVM to JavaScript compiler"
+    )
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/kripken/emscripten"
     topics = ("emsdk", "emscripten", "sdk")
@@ -38,8 +40,12 @@ class EmSDKConan(ConanFile):
         del self.info.settings.build_type
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=True)
+        get(
+            self,
+            **self.conan_data["sources"][self.version],
+            destination=self.source_folder,
+            strip_root=True,
+        )
 
     @property
     def _relative_paths(self):
@@ -47,7 +53,11 @@ class EmSDKConan(ConanFile):
 
     @property
     def _paths(self):
-        return [os.path.join(self.package_folder, path) for path in self._relative_paths]
+        print(self._relative_paths)
+        print(self.package_folder)
+        return [
+            os.path.join(self.package_folder, path) for path in self._relative_paths
+        ]
 
     @property
     def _emsdk(self):
@@ -82,7 +92,9 @@ class EmSDKConan(ConanFile):
     def _tools_for_version(self):
         ret = {}
         # Select release-upstream from version (wasm-binaries)
-        with open(os.path.join(self.source_folder, "emscripten-releases-tags.json"), "r") as f:
+        with open(
+            os.path.join(self.source_folder, "emscripten-releases-tags.json"), "r"
+        ) as f:
             data = json.load(f)
             ret["wasm"] = f"releases-upstream-{data['releases'][self.version]}-64bit"
         # Select python and node versions
@@ -90,9 +102,23 @@ class EmSDKConan(ConanFile):
             data = json.load(f)
             tools = data["tools"]
             if self.settings.os == "Windows":
-                python = next((it for it in tools if (it["id"] == "python" and not it.get("is_old", False))), None)
+                python = next(
+                    (
+                        it
+                        for it in tools
+                        if (it["id"] == "python" and not it.get("is_old", False))
+                    ),
+                    None,
+                )
                 ret["python"] = f"python-{python['version']}-64bit"
-            node = next((it for it in tools if (it["id"] == "node" and not it.get("is_old", False))), None)
+            node = next(
+                (
+                    it
+                    for it in tools
+                    if (it["id"] == "node" and not it.get("is_old", False))
+                ),
+                None,
+            )
             ret["nodejs"] = f"node-{node['version']}-64bit"
         return ret
 
@@ -104,28 +130,51 @@ class EmSDKConan(ConanFile):
             # Install required tools
             required_tools = self._tools_for_version()
             for key, value in required_tools.items():
-                if key != 'nodejs':
+                if key != "nodejs":
                     self.run(f"{emsdk} install {value}")
                     self.run(f"{emsdk} activate {value}")
 
     def package(self):
-        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
-        copy(self, "*", src=self.source_folder, dst=os.path.join(self.package_folder, "bin"))
+        copy(
+            self,
+            "LICENSE",
+            src=self.source_folder,
+            dst=os.path.join(self.package_folder, "licenses"),
+        )
+        copy(
+            self,
+            "*",
+            src=self.source_folder,
+            dst=os.path.join(self.package_folder, "bin"),
+        )
         emscripten = os.path.join(self.package_folder, "bin", "upstream", "emscripten")
-        toolchain = os.path.join(emscripten, "cmake", "Modules", "Platform", "Emscripten.cmake")
+        toolchain = os.path.join(
+            emscripten, "cmake", "Modules", "Platform", "Emscripten.cmake"
+        )
         # FIXME: conan should add the root of conan package requirements to CMAKE_PREFIX_PATH (LIBRARY/INCLUDE -> ONLY; PROGRAM -> NEVER)
         # allow to find conan libraries
-        replace_in_file(self, toolchain,
-                              "set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)",
-                              "set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)")
-        replace_in_file(self, toolchain,
-                              "set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)",
-                              "set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)")
-        replace_in_file(self, toolchain,
-                              "set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)",
-                              "set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)")
+        replace_in_file(
+            self,
+            toolchain,
+            "set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)",
+            "set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)",
+        )
+        replace_in_file(
+            self,
+            toolchain,
+            "set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)",
+            "set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)",
+        )
+        replace_in_file(
+            self,
+            toolchain,
+            "set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)",
+            "set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)",
+        )
         if not cross_building(self):
-            self.run("embuilder build MINIMAL", env=["conanemsdk", "conanrun"]) # force cache population
+            self.run(
+                "embuilder build MINIMAL", env=["conanemsdk", "conanrun"]
+            )  # force cache population
             # the line below forces emscripten to accept the cache as-is, even after re-location
             # https://github.com/emscripten-core/emscripten/issues/15053#issuecomment-920950710
             os.remove(os.path.join(self._em_cache, "sanity.txt"))
@@ -148,16 +197,27 @@ class EmSDKConan(ConanFile):
             return
 
         if self.settings_target.os != "Emscripten":
-            self.output.warning(f"You've added {self.name}/{self.version} as a build requirement, while os={self.settings_target.os} != Emscripten")
+            self.output.warning(
+                f"You've added {self.name}/{self.version} as a build requirement, while os={self.settings_target.os} != Emscripten"
+            )
             return
 
-        toolchain = os.path.join(self.package_folder, "bin", "upstream", "emscripten", "cmake", "Modules", "Platform", "Emscripten.cmake")
+        toolchain = os.path.join(
+            self.package_folder,
+            "bin",
+            "upstream",
+            "emscripten",
+            "cmake",
+            "Modules",
+            "Platform",
+            "Emscripten.cmake",
+        )
         self.conf_info.prepend("tools.cmake.cmaketoolchain:user_toolchain", toolchain)
 
         self.buildenv_info.define_path("EMSDK", self._emsdk)
         self.buildenv_info.define_path("EMSCRIPTEN", self._emscripten)
         self.buildenv_info.define_path("EM_CONFIG", self._em_config)
-        self.buildenv_info.define_path("EM_CACHE",  self._em_cache)
+        self.buildenv_info.define_path("EM_CACHE", self._em_cache)
 
         compiler_executables = {
             "c": self._define_tool_var("emcc"),
@@ -174,10 +234,25 @@ class EmSDKConan(ConanFile):
         self.cpp_info.builddirs = [
             os.path.join("bin", "releases", "src"),
             os.path.join("bin", "upstream", "emscripten", "cmake", "Modules"),
-            os.path.join("bin", "upstream", "emscripten", "cmake", "Modules", "Platform"),
-            os.path.join("bin", "upstream", "emscripten", "system", "lib", "libunwind", "cmake", "Modules"),
-            os.path.join("bin", "upstream", "emscripten", "system", "lib", "libunwind", "cmake"),
-            os.path.join("bin", "upstream", "emscripten", "tests", "cmake", "target_library"),
+            os.path.join(
+                "bin", "upstream", "emscripten", "cmake", "Modules", "Platform"
+            ),
+            os.path.join(
+                "bin",
+                "upstream",
+                "emscripten",
+                "system",
+                "lib",
+                "libunwind",
+                "cmake",
+                "Modules",
+            ),
+            os.path.join(
+                "bin", "upstream", "emscripten", "system", "lib", "libunwind", "cmake"
+            ),
+            os.path.join(
+                "bin", "upstream", "emscripten", "tests", "cmake", "target_library"
+            ),
             os.path.join("bin", "upstream", "lib", "cmake", "llvm"),
         ]
 
